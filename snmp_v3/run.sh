aa2c081bc815c17d2b4ec323ac807f74e0f08605
Dog-Gone-Earl <terry.dennison@datadoghq.com>
2024-01-20 17:41:03 -0700

    update snmp sandboxes

--- snmp_v3/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $(arch) == "arm64" ]]; then
-  echo "Running VAGRANT_VAGRANTFILE=Vagrantfile.arm64 vagrant $1"
-  VAGRANT_VAGRANTFILE=Vagrantfile.arm64 vagrant $1
-else
-  echo "Running VAGRANT_VAGRANTFILE=Vagrantfile.intel vagrant $1"
-  VAGRANT_VAGRANTFILE=Vagrantfile.intel vagrant $1
-fi
