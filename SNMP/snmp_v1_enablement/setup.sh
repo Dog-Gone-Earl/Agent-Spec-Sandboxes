f09463b058fded2464443edc2f2e66a27352a3d9
Terry Dennison Jr <107069502+Dog-Gone-Earl@users.noreply.github.com>
2024-06-27 18:26:24 -0600

    Delete SNMP/snmp_v1_enablement directory

--- SNMP/snmp_v1_enablement/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-comm_string=<VALUE>
-echo "Provisioning!"
-sudo apt-get update -y; sudo apt-get upgrade -y; sudo apt-get install -y snmpd snmp
-sudo sed -i '73i rocommunity '$comm_string'' /etc/snmp/snmpd.conf
-sudo sed -i "s/agentaddress  127.0.0.1,[::1]/agentAddress udp:161,udp6:[::1]:161/1" /etc/snmp/snmpd.conf
-sudo service snmpd restart
