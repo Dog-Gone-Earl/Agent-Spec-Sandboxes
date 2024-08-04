1443d7c84189e88ed572140050e24ab991c49a42
Terry Dennison Jr <107069502+Dog-Gone-Earl@users.noreply.github.com>
2024-08-04 11:45:49 -0600

    Update setup.sh

--- Integrations/flink/setup.sh
+++ Integrations/flink/setup.sh
@@ -7,10 +7,10 @@ sudo apt install python -y
 wget -o flink-data.tgz https://dlcdn.apache.org/flink/flink-1.18.1/flink-1.18.1-bin-scala_2.12.tgz
 tar -xzf flink-1.18.1-bin-scala_2.12.tgz 
 
-sudo apt install default-jdk
-sudo apt update;sudo apt install default-jre
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:linuxuprising/java -y
-sudo apt install oracle-java17-installer
+sudo apt install default-jdk -y
+sudo apt update
+sudo apt install default-jre software-properties-common -y
+sudo add-apt-repository ppa:linuxuprising/java
+sudo apt install oracle-java17-installer -y
 sudo apt-get update
 
